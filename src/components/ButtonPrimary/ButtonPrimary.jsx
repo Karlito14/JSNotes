932d2dd3207baeafd4719a5447842d4258b15ef3
Karlito14 <leiroz26@hotmail.com>
2024-03-09 17:42:26 +0100

    bouton disabled

--- src/components/ButtonPrimary/ButtonPrimary.jsx
+++ src/components/ButtonPrimary/ButtonPrimary.jsx
@@ -1,7 +1,7 @@
 import style from './style.module.scss';
 
-export const ButtonPrimary = ({ text, onClick }) => {
+export const ButtonPrimary = ({ text, onClick, isDisabled }) => {
     return (
-        <button onClick={onClick} type='button' className={style.btn}>{text}</button>
+        <button onClick={onClick} type='button' disabled={isDisabled} className={style.btn}>{text}</button>
     )
 }
